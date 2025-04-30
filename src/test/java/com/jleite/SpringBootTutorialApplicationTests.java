8884bbef7ccc42a586fbad081413fa9288af1ee4
Jaqueline Araujo <44411257+jaquelinearaujo@users.noreply.github.com>
2025-04-30 08:56:45 +0100

    first commit

---
+++ src/test/java/com/jleite/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jleite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
